fa6689c1155ef244e88d18ebab9df212238c67b7
Lohit0209 <lohit.divakarla@gmail.com>
2026-02-01 00:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -396,8 +396,8 @@ st.markdown("""
 
 /* Logo sizing — NORMAL, PROFESSIONAL */
 .nexus-header img {
-    height: 35px;   /* <-- correct logo size */
-    width: auto;
+    height: 100px;   /* <-- correct logo size */
+    width: 200px;
 }
 
 /* Title text next to logo */
